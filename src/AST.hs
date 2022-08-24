e1c618c6a92db541d1eb01b9683e0caa5e55e444
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-08-25 00:24:10 +0200

    added AST

---
+++ src/AST.hs
@@ -0,0 +1,50 @@
+module AST where
+
+import Control.Applicative ((<|>))
+
+data Expr
+  = Var Char
+  | And Expr Expr
+  | Or Expr Expr
+  | Not Expr
+  | Const Bool
+  deriving (Show, Eq)
+
+freeVariable :: Expr -> Maybe Char
+freeVariable (Const _) = Nothing
+freeVariable (Var v) = Just v
+freeVariable (Not e) = freeVariable e
+freeVariable (Or x y) = freeVariable x <|> freeVariable y
+freeVariable (And x y) = freeVariable x <|> freeVariable y
+
+guessVariable :: Char -> Bool -> Expr -> Expr
+guessVariable v b e = case e of
+  Const b -> Const b
+  Not e -> Not $ guessVariable v b e
+  Or l r -> Or (guessVariable v b l) (guessVariable v b r)
+  And l r -> And (guessVariable v b l) (guessVariable v b r)
+  Var c ->
+    if v == c
+      then Const b
+      else Var v
+
+simplify :: Expr -> Expr
+simplify (Const b) = (Const b)
+simplify (Not (Const b)) = Const (not b)
+simplify (Not e) = Not e
+simplify (Or x y) =
+  let es = filter (/= Const False) [simplify x, simplify y]
+   in if Const True `elem` es
+        then Const True
+        else case es of
+          [] -> Const False
+          [e] -> e
+          [e1, e2] -> Or e1 e2
+simplify (And x y) =
+  let es = filter (/= Const True) [simplify x, simplify y]
+   in if Const False `elem` es
+        then Const False
+        else case es of
+          [] -> Const True
+          [e] -> e
+          [e1, e2] -> And e1 e2
